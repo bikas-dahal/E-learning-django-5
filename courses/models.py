a20b0452b0abb0aa3d14d0dddffa4fe06d976a37
bikky <bikkydahal@gmail.com>
2024-05-27 13:56:04 +0545

    Basic authentication along with custom model added

--- courses/models.py
+++ courses/models.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from .fields import OrderField
 
 
 class Subject(models.Model):
@@ -38,6 +41,54 @@ class Module(models.Model):
         Course, related_name='modules', on_delete=models.CASCADE
     )
     title = models.CharField(max_length=200)
+    order = OrderField(blank=True, for_fields=['course'])
     description = models.TextField(blank=True)
     def __str__(self):
+        return f'{self.order}. {self.title}'
+    
+    class Meta:
+        ordering = ['order']
+    
+
+class ItemBase(models.Model):
+    owner = models.ForeignKey(User,
+        related_name='%(class)s_related',
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    class Meta:
+        abstract = True
+    def __str__(self):
         return self.title
+class Text(ItemBase):
+    content = models.TextField()
+class File(ItemBase):
+    file = models.FileField(upload_to='files')
+class Image(ItemBase):
+    file = models.FileField(upload_to='images')
+class Video(ItemBase):
+    url = models.URLField()
+    
+    
+
+class Content(models.Model):
+    module = models.ForeignKey(
+        Module,
+        related_name='contents',
+        on_delete=models.CASCADE
+    )
+    content_type = models.ForeignKey(
+        ContentType,
+        on_delete=models.CASCADE,
+        limit_choices_to={
+            'model__in':('text', 'video', 'image', 'file')
+        }
+    ) 
+    order = OrderField(blank=True, for_fields=['module'])
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey('content_type', 'object_id')
+    
+    class Meta:
+        ordering = ['order']
--- educa/settings.py
+++ educa/settings.py
@@ -125,3 +125,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+
